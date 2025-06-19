f1f16066e23229eb18c29a27c70fdbda61130974
Md Aminul Islam <mr.aminul118@gmail.com>
2025-06-20 01:21:52 +0600

    Book interface and model create

---
+++ src/app/modules/book/book.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import { IBook } from "./book.interface";
+
+const bookSchema = new Schema<IBook>({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
+  author: {
+    type: String,
+    required: [true, "Author  is required"],
+  },
+  isbn: {
+    type: String,
+    required: [true, "ISBN is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required"],
+  },
+  copies: {
+    type: Number,
+    min: 0,
+    required: [true, "Copies required"],
+  },
+  available: {
+    type: Boolean,
+    required: [true, "Available is required"],
+  },
+});
+
+const Book = mongoose.model<IBook>("Book", bookSchema);
+
+export default Book;

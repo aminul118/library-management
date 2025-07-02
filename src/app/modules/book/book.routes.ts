92567458f2a265cf5b99a1e996e82fbc52fb7abf
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-07-03 00:10:47 +0600

    book update method change

--- src/app/modules/book/book.routes.ts
+++ src/app/modules/book/book.routes.ts
@@ -6,7 +6,7 @@ const bookRouter = Router();
 bookRouter.post("/books", BookController.createBook);
 bookRouter.get("/books", BookController.getBooks);
 bookRouter.get("/books/:bookId", BookController.getBookById);
-bookRouter.patch("/books/:bookId", BookController.updateBook);
+bookRouter.put("/books/:bookId", BookController.updateBook);
 bookRouter.delete("/books/:bookId", BookController.deleteBook);
 
 export default bookRouter;
